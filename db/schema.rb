c4788cb2cfa72b6dc848280eaefb766aed0a9b6c
ao2137 <allison.oldfather@gmail.com>
2017-07-12 08:14:05 -0400

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170711012715) do
+ActiveRecord::Schema.define(version: 20170711015247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170711012715) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 
   create_table "skills", force: :cascade do |t|
     t.string "title"
